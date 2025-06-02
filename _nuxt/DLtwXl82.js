425ed240af2c448da26b3d695ee8907e98eeaeef
CHUN-WEI-HSU <CHUN-WEI-HSU@users.noreply.github.com>
2025-06-02 14:01:38 +0000

    deploy: CHUN-WEI-HSU/yilan-land-dev@bd19f11deef19123c43a9afd42dde6366ad47d01

--- _nuxt/DLtwXl82.js
+++
@@ -1,1 +0,0 @@
-import{v as t,C as e,Z as s,an as n}from"./X6HiR9a0.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
